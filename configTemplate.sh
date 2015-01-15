0f74aeb61b4bf8777c8f3234aeeb97aaa48a4aae
Michael Hadorn <michael.hadorn@laupercomputing.com>
2015-01-16 00:20:36 +0100

    add: configTemplate (so live config can used without git)

---
+++ configTemplate.sh
@@ -0,0 +1,45 @@
+# BEGIN CONFIGURATION ==========================================================
+
+BACKUP_DIR="/backups/site_backups"  # The directory in which you want backups placed
+DUMP_MYSQL=true
+TAR_SITES=false
+SYNC="none" # Either 's3sync', 'rsync', or 'none'
+KEEP_MYSQL="14" # How many days worth of mysql dumps to keep
+KEEP_SITES="2" # How many days worth of site tarballs to keep
+
+MYSQL_HOST="localhost"
+MYSQL_USER="root"
+MYSQL_PASS=""
+MYSQL_BACKUP_DIR="$BACKUP_DIR/mysql/"
+MYSQL_DATABASE="myDataBase" # which database to backup, if this is empty all databases will be back uped
+
+SITES_DIR="/var/www/sites/"
+SITES_BACKUP_DIR="$BACKUP_DIR/sites/"
+SITES_EXCLUDES = ""
+
+
+# See s3sync info in README
+S3SYNC_PATH="/usr/local/s3sync/s3sync.rb"
+S3_BUCKET="my-fancy-bucket"
+AWS_ACCESS_KEY_ID="YourAWSAccessKey" # Log in to your Amazon AWS account to get this
+AWS_SECRET_ACCESS_KEY="YourAWSSecretAccessKey" # Log in to your Amazon AWS account to get this
+USE_SSL="true"
+SSL_CERT_DIR="/etc/ssl/certs" # Where your Cert Authority keys live; for verification
+SSL_CERT_FILE="" # If you have just one PEM file for CA verification
+
+# If you don't want to use S3, you can rsync to another server
+RSYNC_USER="user"
+RSYNC_SERVER="other.server.com"
+RSYNC_DIR="web_site_backups"
+RSYNC_PORT="22" # Change this if you've customized the SSH port of your backup system
+
+# You probably won't have to change these
+THE_DATE="$(date '+%Y-%m-%d')"
+
+MYSQL_PATH="$(which mysql)"
+MYSQLDUMP_PATH="$(which mysqldump)"
+FIND_PATH="$(which find)"
+TAR_PATH="$(which tar)"
+RSYNC_PATH="$(which rsync)"
+
+# END CONFIGURATION ============================================================
